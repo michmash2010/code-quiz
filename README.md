8037b8accddde786a3dda4ec021a7b87a8f8965b
michmash2010 <michellecrowe2010@gmail.com>
2021-09-19 23:15:57 -0400

    add first question/choice text using javascript, add README text, add screenshot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# code-quiz
+# Challenge 4: Code Quiz
+for UCF Coding Bootcamp
+
+## Link to the Live Page
+<https://michmash2010.github.io/code-quiz/>
+
+## Description
+This quiz page currently in development features JavaScript in addition to HTML and CSS.
+
+## Portfolio Webpage Screenshot
+![A screenshot of the finished Code Quiz webpage, zoomed-out to show full length and layout formatting](./assets/images/code-quiz.PNG)
+
+## Credits
+A mock-up for the basic design/appearance of the quiz and the example questions were included in the assignment.
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-var questions = 
+var questionEl = document.querySelector("#question");
+var choice1El = document.querySelector("#choice1");
+var choice2El = document.querySelector("#choice2");
+var choice3El = document.querySelector("#choice3");
+var choice4El = document.querySelector("#choice4");
+
+
+var questionArray = 
 [
+    {
+        question: "Coding Quiz Challenge",
+        choices: ["Start Quiz"],
+        answer: 1
+    },
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["1. strings","2. booleans","3. alerts","4. numbers"],
@@ -17,9 +29,50 @@ var questions =
     }
 ];
 
+var questionHandler = function() {
+    // Add the question to the question block
+    for (i = 1; i < questionArray.length; i++) {
+        questionEl.textContent = questionArray[i].question;
+        choice1El.textContent = questionArray[i].choices[0];
+        choice2El.textContent = questionArray[i].choices[1];
+        choice3El.textContent = questionArray[i].choices[2];
+        choice4El.textContent = questionArray[i].choices[3];
+        //setTimeout(function(){ "Hello!" }, 3000);
+    }
+};
+
+questionHandler();
+
+// Examples
+console.log(questionArray[0]);  // logs the whole contents of the array at that index
+console.log(questionArray[2].answer);  // logs just the answer #
+
+// get the element that contains the question text, and set it to a question
+var questionEl = document.querySelector("#question");
+
+
+// get the element that contains the first choice text, and set it to that choice
+    // choice1El = document.querySelector("#choice1");
+    // choice1El.textContent = questionArray[2].choices[0];
+
+// Pseudocode for the quiz sequence of events:
+// User clicks a start button on a welcome page (does this mean the buttons need to be dynamically generated?)
+
+// Timer starts counting down
+// First question and answer choices display
+
+// User clicks an answer choice button
+// Event listeners pick up the click and compare the choice to the answer.
+// If choice === answer, display "Correct!" 
+// else display "Incorrect." and subtract 10 seconds from the timer.
+// After a 3 second delay, display the next question
+
+// When all questions are complete, or timer runs out, display the score form page.
+
+
 
 var timerEl = document.querySelector("#timer");
-var timer = 60
+var timer = 60;
 var countdown = function() {
     //console.log(timer);
     timerEl.textContent = "Time: " + timer;
@@ -31,5 +84,3 @@ var countdown = function() {
 };
 
 var startTimer = setInterval(countdown, 1000);
-
-
