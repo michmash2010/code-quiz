61b68d9dcb672e5bbd1a2efaf53f1dd5e2717773
michmash2010 <michellecrowe2010@gmail.com>
2021-09-19 21:14:17 -0400

    add initial question/choice/answer array and pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-
-
-
-
-
+var questions = 
+[
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["1. strings","2. booleans","3. alerts","4. numbers"],
+        answer: 3
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ________.",
+        choices: ["1. quotes","2. curly brackets","3. parenthesis","4. square brackets"],
+        answer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript","2. Terminal/GitBash","3. a for loop","4. console.log"],
+        answer: 4
+    }
+];
 
 
 var timerEl = document.querySelector("#timer");
-var timer = 10
+var timer = 60
 var countdown = function() {
     //console.log(timer);
     timerEl.textContent = "Time: " + timer;
